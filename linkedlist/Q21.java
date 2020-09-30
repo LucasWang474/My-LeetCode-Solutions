7f0818ff4c61628041704d10634681c625488089
lucas <474.lucas.wang@gmail.com>
2020-09-30 09:38:44 +0800

    Finished Q21. Merge Two Sorted Lists

---
+++ linkedlist/Q21.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/
+ */
+public class Q21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0), ptr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                ptr.next = l1;
+                l1 = l1.next;
+            } else {
+                ptr.next = l2;
+                l2 = l2.next;
+            }
+            ptr = ptr.next;
+        }
+        ptr.next = (l1 != null) ? l1 : l2;
+
+        return dummy.next;
+    }
+}
