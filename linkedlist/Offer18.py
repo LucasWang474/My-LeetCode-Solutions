41aea9772112c9a5b4731ad80d533a5ed0ccd47e
Lucas Wang <474.lucas.wang@gmail.com>
2021-03-15 13:32:34 +0800

    Update

---
+++ linkedlist/Offer18.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    """
+    https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
+    """
+
+    def deleteNode(self, head: ListNode, val: int) -> ListNode:
+        if not head:
+            return None
+        if head.val == val:
+            return head.next
+        
+        pre, cur = head, head.next
+        while cur and cur.val != val:
+            pre, cur = cur, cur.next
+        if cur:
+            pre.next = cur.next
+        return head
