396b6e3611e83d995fa6ca8968fd4d99c9865735
Lucas Wang <474.lucas.wang@gmail.com>
2021-06-05 12:35:43 +0800

    Finish 24. Swap Nodes in Pairs

---
+++ linkedlist/Q24.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/swap-nodes-in-pairs/
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        ptr = dummy = ListNode(0, head)
+        while ptr.next and ptr.next.next:
+            first = ptr.next
+            second = first.next
+            nextPtr = second.next
+
+            first.next = nextPtr
+            second.next = first
+            ptr.next = second
+            ptr = first
+        return dummy.next
