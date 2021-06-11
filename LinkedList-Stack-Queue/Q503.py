d3342815ea10416d0b54ab77377f50e9480381ae
Lucas Wang <474.lucas.wang@gmail.com>
2021-06-11 16:18:50 +0800

    Finish 503. Next Greater Element II

--- LinkedList-Stack-Queue/Q503.py
+++ LinkedList-Stack-Queue/Q503.py
@@ -1,4 +1,15 @@
 # https://leetcode.com/problems/next-greater-element-ii/
 
 class Solution:
-    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+    def nextGreaterElements(self, nums):
+        N = len(nums)
+        res, stack = [-1] * N, []
+
+        for i in range(N * 2):
+            i %= N
+            cur = nums[i]
+            while stack and nums[stack[-1]] < cur:
+                res[stack.pop()] = cur
+            stack.append(i)
+
+        return res
