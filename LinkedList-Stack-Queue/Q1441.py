a255fd52bfb99f2a3064d48cf32d3f0900cf1ac4
Lucas Wang <474.lucas.wang@gmail.com>
2021-06-17 13:05:51 +0800

    Finish 1441. Build an Array With Stack Operations

---
+++ LinkedList-Stack-Queue/Q1441.py
@@ -0,0 +1,14 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        res = []
+        i = 0
+        for cur in range(1, n + 1):
+            if i == len(target):
+                break
+
+            res.append("Push")
+            if cur == target[i]:
+                i += 1
+            elif cur < target[i]:
+                res.append("Pop")
+        return res
