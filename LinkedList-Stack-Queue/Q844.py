49278f63c018f9956f26a39b0ba08626fce03099
Lucas Wang <474.lucas.wang@gmail.com>
2021-06-18 08:45:31 +0800

    Update 844. Backspace String Compare

--- LinkedList-Stack-Queue/Q844.py
+++ LinkedList-Stack-Queue/Q844.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/backspace-string-compare/
+import itertools
+
 
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
@@ -24,3 +26,28 @@ class Solution:
             return stack
 
         return to_stack(s) == to_stack(t)
+
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        """
+        >>> sol = Solution()
+        >>> sol.backspaceCompare("ab#c", "ad#c")
+        True
+        >>> sol.backspaceCompare("ab##", "c#d#")
+        True
+        >>> sol.backspaceCompare("a##c", "#a#c")
+        True
+        >>> sol.backspaceCompare("a#c", "b")
+        False
+        """
+
+        def helper(string: str):
+            skip = 0
+            for c in reversed(string):
+                if c == "#":
+                    skip += 1
+                elif skip > 0:
+                    skip -= 1
+                else:
+                    yield c
+
+        return all(x == y for x, y in itertools.zip_longest(helper(s), helper(t)))
