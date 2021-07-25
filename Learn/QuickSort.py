516ac538eb7bf93be9118aaf2379710509aac040
Lucas Wang <474.lucas.wang@gmail.com>
2021-07-26 07:48:20 +0800

    Add QuickSelect Implementation

--- Learn/QuickSort.py
+++ Learn/QuickSort.py
@@ -1,21 +1,20 @@
-def partition(arr, lo, hi):
-    i, j, pivot = lo, hi + 1, arr[lo]
-    while True:
-        i += 1
-        while arr[i] < pivot and i < hi:
-            i += 1
+def select(arr, k):
+    """
+    >>> arr = [3, 2, 1, -1, -2, -3, 4, 5, 6, 9, 8, 7]
+    >>> select(arr, 5)
+    3
+    """
+    lo, hi = 0, len(arr) - 1
 
-        j -= 1
-        while arr[j] > pivot:
-            j -= 1
-
-        if j <= i:
+    while hi > lo:
+        j = partition(arr, lo, hi)
+        if k < j:
+            hi = j - 1
+        elif k > j:
+            lo = j + 1
+        else:
             break
-
-        arr[i], arr[j] = arr[j], arr[i]
-
-    arr[lo], arr[j] = arr[j], arr[lo]
-    return j
+    return arr[k]
 
 
 def sort(arr):
@@ -39,3 +38,23 @@ def sort_recur(arr, lo, hi):
     j = partition(arr, lo, hi)
     sort_recur(arr, lo, j - 1)
     sort_recur(arr, j + 1, hi)
+
+
+def partition(arr, lo, hi):
+    i, j, pivot = lo, hi + 1, arr[lo]
+    while True:
+        i += 1
+        while arr[i] < pivot and i < hi:
+            i += 1
+
+        j -= 1
+        while arr[j] > pivot:
+            j -= 1
+
+        if j <= i:
+            break
+
+        arr[i], arr[j] = arr[j], arr[i]
+
+    arr[lo], arr[j] = arr[j], arr[lo]
+    return j
