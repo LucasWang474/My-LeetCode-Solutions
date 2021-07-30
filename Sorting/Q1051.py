79e1021350554f35efff869e4435af29be4c8328
Lucas Wang <474.lucas.wang@gmail.com>
2021-07-30 12:48:49 +0800

    Finish 1051. Height Checker

---
+++ Sorting/Q1051.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/height-checker/
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+        return sum(h1 != h2 for h1, h2 in zip(heights, expected))
